5c77a218b575c75697840911c052d3744c30514f
erica <>
2019-05-31 20:35:59 +0800

    scroll tabs

--- webroot/scripts/main.js
+++ webroot/scripts/main.js
@@ -38,26 +38,26 @@ $(function () {
       };
     },
     handleElementsInView: function ($elements) {
-      var window_height = $(window).height();
-      var window_top_position = $(window).scrollTop();
-      var window_bottom_position = window_top_position + window_height;
+      // var window_height = $(window).height();
+      // var window_top_position = $(window).scrollTop();
+      // var window_bottom_position = window_top_position + window_height;
   
-      $.each($elements, function () {
-        var $element = $(this);
-        var element_height = $element.innerHeight();
-        var element_top_position = $element.offset().top;
-        var element_bottom_position = element_top_position + element_height;
+      // $.each($elements, function () {
+      //   var $element = $(this);
+      //   var element_height = $element.innerHeight();
+      //   var element_top_position = $element.offset().top;
+      //   var element_bottom_position = element_top_position + element_height;
   
-        // check to see if this current container is within viewport
-        if (
-          (element_bottom_position >= window_top_position) &&
-          (element_top_position <= window_bottom_position)
-        ) {
-          $element.addClass('in-view');
-        } else {
-          $element.removeClass('in-view');
-        }
-      });
+      //   // check to see if this current container is within viewport
+      //   if (
+      //     (element_bottom_position >= window_top_position) &&
+      //     (element_top_position <= window_bottom_position)
+      //   ) {
+      //     $element.addClass('in-view');
+      //   } else {
+      //     $element.removeClass('in-view');
+      //   }
+      // });
     },
     handleClickHash: function (event) {
       var target = $(this.getAttribute('href'));
@@ -71,21 +71,47 @@ $(function () {
       }
     },
     handleClickTabs: function (event) {
-      event.preventDefault();
+      // event.preventDefault();
   
-      $('#tabs a').removeClass('active');
-      $(this).addClass('active');
-      $('.contBox').hide();
-      $($(this).attr('href')).fadeIn(300);
+      // $('#tabs a').removeClass('active');
+      // $(this).addClass('active');
+      // $('.contBox').hide();
+      // $($(this).attr('href')).fadeIn(300);
 
-      var tabTarget = $(this.getAttribute('href'));
-      var anchorSpace = Number($("#nav.fixed").innerHeight() + $("#tabs").innerHeight());
-      if (tabTarget.length) {
+      // var tabTarget = $(this.getAttribute('href'));
+      // var anchorSpace = $("#nav.fixed").innerHeight() + $("#tabs").innerHeight();
+      // if (tabTarget.length) {
+      //   $('html, body').stop().animate({
+      //     scrollTop: $("#contBox").offset().top - anchorSpace
+      //   }, 500);
+      //   return false;
+      // }
+    },
+    handleTabScroll: function (event) {
+      var anchorSpace = $("#nav.fixed").innerHeight() + $("#tabs").innerHeight();
+      //smoothscroll
+      $('#tabs a').on('click', function () {
+        var target = this.hash;
+        $target = $(target);
+        
         $('html, body').stop().animate({
-          scrollTop: $("#contBox").offset().top - anchorSpace
-        }, 500);
+          'scrollTop': $target.offset().top - anchorSpace + 2
+        }, 800);
         return false;
-      }
+      });
+
+      var scrollPos = $(document).scrollTop();
+      $('#tabs a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+
+        if ( scrollPos >= refElement.offset().top - anchorSpace
+          && scrollPos <= refElement.offset().top + refElement.innerHeight() - anchorSpace) {
+          $('#tabs a').removeClass("active");
+          currLink.addClass("active");
+        }
+        
+      });
     }
   }
 
@@ -93,12 +119,13 @@ $(function () {
     $(window).on('scroll', function () {
       methods.handleNavFixed()
       methods.handleTabsFixed()
+      methods.handleTabScroll()
     });
     $(window).on('scroll resize', function () {
-      methods.handleElementsInView($('.bkBox'))
+      // methods.handleElementsInView($('.bkBox'))
     });
     $('a[href^="#"].not[href="#"]').on('click', methods.handleClickHash);
-    $('#tabs a').on('click', methods.handleClickTabs);
+    // $('#tabs a').on('click', methods.handleClickTabs);
   }
 
   var beforeunload = function () {
