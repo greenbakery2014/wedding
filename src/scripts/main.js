456495ddba3421cc058ea833d05902ae85e56cc0
erica <>
2019-06-28 18:45:09 +0800

    giftbox page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -38,31 +38,8 @@ $(function () {
         $("#tabs").removeClass("fixed")
       };
     },
-    handleElementsInView: function ($elements) {
-      // var window_height = $(window).height();
-      // var window_top_position = $(window).scrollTop();
-      // var window_bottom_position = window_top_position + window_height;
-  
-      // $.each($elements, function () {
-      //   var $element = $(this);
-      //   var element_height = $element.innerHeight();
-      //   var element_top_position = $element.offset().top;
-      //   var element_bottom_position = element_top_position + element_height;
-  
-      //   // check to see if this current container is within viewport
-      //   if (
-      //     (element_bottom_position >= window_top_position) &&
-      //     (element_top_position <= window_bottom_position)
-      //   ) {
-      //     $element.addClass('in-view');
-      //   } else {
-      //     $element.removeClass('in-view');
-      //   }
-      // });
-    },
     handleClickHash: function (event) {
       var target = $(this.getAttribute('href'));
-  
       if (target.length) {
         event.preventDefault();
         $('html, body').stop().animate({
@@ -121,9 +98,6 @@ $(function () {
       // methods.handleNavFixed()
       methods.handleTabsFixed()
       methods.handleTabScroll()
-    });
-    $(window).on('scroll resize', function () {
-      // methods.handleElementsInView($('.bkBox'))
     });
     $('a[href^="#"].not[href="#"]').on('click', methods.handleClickHash);
     // $('#tabs a').on('click', methods.handleClickTabs);
